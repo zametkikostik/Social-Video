6c10d7847e29570f082ee446c6463b45c5809959
zametkikostik <150536839+zametkikostik@users.noreply.github.com>
2026-09-17 13:25:35 +0300

    Federation: HTTP Signatures + fan-out delivery on video READY

--- backend/src/modules/transcoder/transcoder.module.ts
+++ backend/src/modules/transcoder/transcoder.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { TranscoderService } from './transcoder.service';
 import { StorageModule } from '../storage/storage.module';
+import { FederationModule } from '../federation/federation.module';
 
 @Module({
-  imports: [StorageModule],
+  imports: [StorageModule, forwardRef(() => FederationModule)],
   providers: [TranscoderService],
   exports: [TranscoderService],
 })
